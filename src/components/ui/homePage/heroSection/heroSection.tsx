91fbe274fa18c2830804f5b71173c452161131d6
codersrabbi <pcrabbi2020@gmail.com>
2026-05-09 00:33:46 +0600

    herosection content add

--- src/components/ui/homePage/heroSection/heroSection.tsx
+++ src/components/ui/homePage/heroSection/heroSection.tsx
@@ -1,4 +1,12 @@
-import { Box, Container, Stack, Typography } from "@mui/material";
+import { Box, Button, Container, Stack, Typography } from "@mui/material";
+import SellIcon from "@mui/icons-material/Sell";
+import { Playfair_Display } from "next/font/google";
+import Image from "next/image";
+import img from "@/assets/image/codersrabbi.png";
+const playfair = Playfair_Display({
+  subsets: ["latin"],
+  weight: ["700", "900"],
+});
 
 const HeroSection = () => {
   return (
@@ -12,18 +20,57 @@ const HeroSection = () => {
       }}
     >
       <Container>
-        <Box>
-          <Typography variant="h5" component="h1" sx={{ fontWeight: "bold" }}>
-            Look Better. Feel Strong <br />
-            <Box component="span" sx={{ color: "primary.main" }}>
-              Save 30% on Your First Order!
-            </Box>
-          </Typography>
-          <Typography>
-            lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
-            cumque, voluptate, voluptate, voluptate, voluptate, voluptate
-          </Typography>
-        </Box>
+        <Stack
+          direction={{ xs: "column", md: "row" }}
+          spacing={4}
+          alignItems="center"
+          justifyContent="space-between"
+        >
+          <Box>
+            <Typography
+              variant="h3"
+              className={playfair.className}
+              style={{
+                fontWeight: 900,
+                color: "#333", // তোমার পছন্দের কালার দাও
+                lineHeight: 1.2,
+              }}
+            >
+              Look Better. Feel <br /> Stronger.{" "}
+              <Box component="span" sx={{ color: "primary.main" }}>
+                Save 30% <br /> Now
+              </Box>
+            </Typography>
+            <Typography
+              sx={{ fontFamily: "Inter, Circular", lineHeight: 1.5, mt: 2 }}
+            >
+              Get the best fitness equipment and accessories to help you achieve{" "}
+              your fitness goals. Shop now and save big on our top-quality
+              products.
+            </Typography>
+            <Button
+              variant="contained"
+              size="large"
+              endIcon={<SellIcon />}
+              sx={{ mt: 2 }}
+            >
+              Shop Now
+            </Button>
+          </Box>
+          <Box>
+            <Image
+              src={img}
+              alt="Hero Image"
+              style={{
+                width: "450px",
+                height: "auto",
+                borderRadius: "8px",
+                objectFit: "cover",
+                zIndex: 1,
+              }}
+            />
+          </Box>
+        </Stack>
       </Container>
     </Box>
   );
